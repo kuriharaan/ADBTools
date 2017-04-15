ede938438430722a225be62903846d1886f21605
naoya kurihara <kuriharanaoya@gmail.com>
2017-04-15 13:07:37 +0900

    Add dock window.

---
+++ Assets/ADBTools/Editor/MainWindow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace ADBTools
+{
+    public class MainWindow : EditorWindow
+    {
+        [MenuItem("Window/ADBTools")]
+        static void Open()
+        {
+            System.Type inspectorType = System.Type.GetType("UnityEditor.InspectorWindow,UnityEditor.dll");
+            EditorWindow window = EditorWindow.GetWindow<MainWindow>("ADBTools", inspectorType);
+        }
+    }
+}
